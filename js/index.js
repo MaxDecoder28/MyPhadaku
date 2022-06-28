816469827526eb0916419880dc536390990e8fd6
unknown <maxsharmafeb@gmail.com>
2022-06-28 23:46:39 +0530

    first website

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// let security = prompt("Enter your age");
+// if (security > 16) {
+//     console.log("Welcome guys");
+// }
+
+// else{
+//     console.log("Bhag Bhosdike ke");
+// }
+
+let greetings = alert("Welcome to our free open library....");
+console.log(greetings);
